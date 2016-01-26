f038a84e461b25ca64e574635ba5deb30bb8c0a2
Jaken Herman <jch053@shsu.edu>
2016-01-26 17:05:55 -0600

    Create TelescopeStack.cs

---
+++ TelescopeStack.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Telescope_Receiver
+{
+    class TelescopeStack<T>
+    {
+        static void Main()
+        {
+        }
+        public void push(T value)
+        {
+        }
+        public void pop() 
+        { 
+        }
+    }
+}
